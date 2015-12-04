61707173e860dc064fd93e1ad0e7e04b87661832
and1coder <and1coder@gmail.com>
2015-12-04 17:28:40 +0800

    setting sample
    add setting sample

---
+++ config/setting.sample.js
@@ -0,0 +1,32 @@
+module.exports = {
+    ssiServer: {
+        dev: {
+            host: '127.0.0.1',
+            port: 22,
+            username: "xxoo",
+            password: "****",
+            path: "/path/to/dev/ssi/"
+        },
+        test: {
+            host: '6.6.6.6',
+            port: 22,
+            username: "xxoo",
+            password: "****",
+            path: "/path/to/test/ssi/"
+        },
+        pre: {
+            host: '6.6.6.6',
+            port: 22,
+            username: "xxoo",
+            password: "****",
+            path: "/path/to/pre/ssi/"
+        },
+        idc: {
+            host: '6.6.6.6',
+            port: 22,
+            username: "xxoo",
+            password: "****",
+            path: "/path/to/idc/ssi/"
+        }
+    }
+};
